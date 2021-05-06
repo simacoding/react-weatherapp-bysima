581e9176131f4fad63810e414e212aa83706421f
simacoding <77894138+simacoding@users.noreply.github.com>
2021-05-06 17:20:19 +0200

    Imported axios, made api call and formatted the date
    Installed and imported axios; Then made everything for the api call, so that we get the current weather data (only for the defaultCity); Then I formatted the Date.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <Weather defaultCity="Munich" />
       <footer>
         This project is{" "}
         <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,57 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
+import FormattedDate from "./FormattedDate";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+      date: new Date(response.data.dt * 1000),
+    });
+    console.log(response.data);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="Search"
+                placeholder="Enter city..."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="Submit" value="Search" className="button" />
+            </div>
+          </div>
+        </form>
+
         <div className="row">
-          <div className="col-8">
-            <input
-              type="Search"
-              placeholder="Enter city..."
-              className="form-control"
-            />
+          <div className="col-6">
+            <h1>{weatherData.city}</h1>
+            <ul>
+              <li>
+                <FormattedDate date={weatherData.date} />
+              </li>
+              <li>{weatherData.description}</li>
+            </ul>
+            <br />
+            <ul>
+              <li>Humidity: {weatherData.humidity} %</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
           </div>
-          <div className="col-3">
-            <input type="Submit" value="Search" className="button" />
+
+          <div className="col-2">
+            <div className="clearfix">
+              <div className="float-left">
+                <span className="temperature">
+                  {Math.round(weatherData.temperature)}
+                </span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
           </div>
-        </div>
-      </form>
-
-      <div className="row">
-        <div className="col-6">
-          <h1>Munich</h1>
-          <ul>
-            <li>Monday 11:00 am</li>
-            <li>Cloudy</li>
-          </ul>
-          <br />
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
-        </div>
-
-        <div className="col-2">
-          <div className="clearfix">
-            <div className="float-left">
-              <span className="temperature">6</span>
-              <span className="unit">°C</span>
+          <div className="col-2">
+            <div className="clearfix">
+              <img
+                src={weatherData.iconUrl}
+                alt="cloudy"
+                className="float-left"
+              />
             </div>
           </div>
         </div>
-        <div className="col-2">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-              alt="cloudy"
-              className="float-left"
-            />
-          </div>
-        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "641aea60f164eb7376c34eaed6daea65";
+    let city = props.defaultCity;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
