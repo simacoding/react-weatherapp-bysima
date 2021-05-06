888697f67463c44d1f1b7cbb243ecca707bdb7e1
simacoding <77894138+simacoding@users.noreply.github.com>
2021-05-06 16:10:57 +0200

    Fixed some issues and changed a little bit of the layout

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather /> <br />
-      This project is{" "}
-      <a
-        href="https://github.com/simacoding/react-weatherapp-bysima"
-        target="_blank"
-        rel="noreferrer"
-      >
-        open-source coded on GitHub
-      </a>{" "}
-      by Si Ma
+      <Weather />
+      <footer>
+        This project is{" "}
+        <a
+          href="https://github.com/simacoding/react-weatherapp-bysima"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-source coded on GitHub
+        </a>{" "}
+        by Si Ma
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -19,24 +19,29 @@ export default function Weather() {
         </div>
       </form>
       <h1>Munich</h1>
-      <ul>
-        <li>Monday 11:00 am</li>
-        <li>Cloudy</li>
-      </ul>
       <div className="row">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy"
-          />
-          6°C
+          <ul>
+            <li>Monday 11:00 am</li>
+            <li>Cloudy</li>
+          </ul>
         </div>
+        <div className="col-6">6°C</div>
+      </div>
+
+      <div className="row">
         <div className="col-6">
           <ul>
             <li>Precipitation: 15%</li>
             <li>Humidity: 72%</li>
             <li>Wind: 13 km/h</li>
           </ul>
+        </div>
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="cloudy"
+          />
         </div>
       </div>
     </div>
