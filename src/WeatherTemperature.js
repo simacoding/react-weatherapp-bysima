6e69926a4288caf16a207f52806165bb64c4325c
simacoding <77894138+simacoding@users.noreply.github.com>
2021-05-07 08:55:30 +0200

    changed the temperature
    I made the wrong assignments at first. I fixed it so that the the celsius temp is shown, when clicking on C (not the fahrenheit value)

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -20,7 +20,7 @@ export default function WeatherTemperature(props) {
   if (unit === `celsius`) {
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="temperature">{Math.round(props.celsius)}</span>
         <span className="unit">
           °C |{" "}
           <a href="/" onClick={showFahrenheit}>
@@ -32,7 +32,7 @@ export default function WeatherTemperature(props) {
   } else {
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="temperature">{Math.round(fahrenheit())}</span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
             °C
